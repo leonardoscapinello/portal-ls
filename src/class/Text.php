a51dd0d6402699be0922938a4da6289c155d12d1
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2020-04-30 11:17:04 -0300

    fix: utf8 encoding troubles

--- src/class/Text.php
+++ src/class/Text.php
@@ -26,14 +26,18 @@ class Text
 
     public function utf8($string)
     {
+
+        $string = $this->base64_decode($string);
+
         if ($this->isString($string)) {
             if (preg_match('!!u', $string)) {
                 // UTF8 STRING
                 return utf8_decode($string);
             } else {
                 // NOT UTF=8 STRING, SO CONVERT
-                $string = htmlentities($string, ENT_COMPAT, 'ISO-8859-1', true);
-                $string = iconv(mb_detect_encoding($string, "UTF-8"), "UTF-8//TRANSLIT", $string);
+                //$string = htmlentities($string, ENT_COMPAT, 'ISO-8859-1', true);
+                //$string = iconv(mb_detect_encoding($string, "UTF-8"), "UTF-8//TRANSLIT", $string);
+                $string = iconv(mb_detect_encoding($string, "UTF-16BE"), "UTF-8//TRANSLIT", $string);
                 return $string;
             }
         }
