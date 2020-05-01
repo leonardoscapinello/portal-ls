78be7b2c57b831768a2faa2838a361682130b16c
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2020-05-01 20:30:10 -0300

    fix: disable less compile to deploy

--- src/properties/index.php
+++ src/properties/index.php
@@ -77,7 +77,7 @@ $license = new AccountsLicense();
 $blogIntelligence = new BlogIntelligence();
 $numeric = new Numeric();
 
-
+/*
 //DISABLED BECAUSE GOING DO PRODUCTION
 $less = new lessc();
 $less->compileFile(DIRNAME . "../../public/less/stylesheet.less", DIRNAME . "../../public/stylesheet/stylesheet.css");
@@ -96,7 +96,7 @@ $static->setOutputPath(DIRNAME . "../../public/stylesheet/");
 $static->replace("../images/", "../../public/images/");
 $static->replace("../fonts/", "../../public/fonts/");
 $static->minifyStyleSheet("stylesheet");
-
+*/
 
 $next = get_request("next");
 if (not_empty($next)) {
