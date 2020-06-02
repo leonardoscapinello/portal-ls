a944d55cd95d04c8ccb2473b9d84f862394ac6b6
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2020-06-02 16:09:25 -0300

    chore: add exception to webp convert if using safari

--- public/images/_convert-print.php
+++ public/images/_convert-print.php
@@ -9,29 +9,46 @@ require '../../src/vendor/autoload.php';
 use WebPConvert\WebPConvert;
 
 $src = get_request("src", true);
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
 
 if (!notempty($src)) die;
 
 $source = __DIR__ . "/" . $src;
 $destination = __DIR__ . "/webp/" . $src . '.webp';
 
-try {
-    WebPConvert::serveConverted($source, $destination, [
-        'fail' => 'original',     // If failure, serve the original image (source). Other options include 'throw', '404' and 'report'
-        //'show-report' => true,  // Generates a report instead of serving an image
+if (stripos($user_agent, 'Safari') !== false) {
 
-        'serve-image' => [
-            'headers' => [
-                'cache-control' => true,
-                'vary-accept' => true,
+    if (file_exists($source)) {
+        $size = getimagesize($source);
+        $fp = fopen($source, 'rb');
+        if ($size && $fp) {
+            header('Content-Type: ' . $size['mime']);
+            header('Content-Length: ' . filesize($source));
+            fpassthru($fp);
+            exit;
+        }
+    }
+
+} else {
+
+    try {
+        WebPConvert::serveConverted($source, $destination, [
+            'fail' => 'original',     // If failure, serve the original image (source). Other options include 'throw', '404' and 'report'
+            //'show-report' => true,  // Generates a report instead of serving an image
+
+            'serve-image' => [
+                'headers' => [
+                    'cache-control' => true,
+                    'vary-accept' => true,
+                ],
+                'cache-control-header' => 'max-age=2',
             ],
-            'cache-control-header' => 'max-age=2',
-        ],
 
-        'convert' => [
-            // all convert option can be entered here (ie "quality")
-        ],
-    ]);
-} catch (Exception $exception) {
-    echo $exception->getMessage();
+            'convert' => [
+                // all convert option can be entered here (ie "quality")
+            ],
+        ]);
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
+    }
 }
