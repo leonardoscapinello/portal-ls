7eb59017ed1f796a7f5df98eb8ac1799d4fdf397
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2020-06-11 19:05:40 -0300

    chore: replace all images to webp format

--- src/applications/blog/posts-by-category.php
+++ src/applications/blog/posts-by-category.php
@@ -21,9 +21,12 @@ $list = $contents->getContentsList("ct.id_category = " . $category['id_category'
                                 <div class="row">
                                     <div class="col-xl-2 col-lg-2 col-sm-12">
                                         <figure>
-                                            <img src="<?= $static->loadBlog($list[$i]['cover_image']) ?>"
+											<picture>
+												<source srcset="<?= $static->loadBlog($list[$i]['cover_image']) ?>" type="image/webp">
+												<img src="<?= $static->loadBlog($list[$i]['cover_image']) ?>"
                                                  alt="<?= $list[$i]['title'] ?>"
                                                  style="-webkit-border-radius: 5px;-moz-border-radius: 5px;border-radius: 5px;">
+												 </picture>
                                         </figure>
                                     </div>
                                     <div class="col-xl-8 col-lg-8 col-sm-12">
